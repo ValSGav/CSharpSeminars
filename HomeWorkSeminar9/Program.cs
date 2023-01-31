92768b8b4f5b1480207d83d479b6903099cccd72
ValSGav <valentin-s-gavrilov@yandex.ru>
2023-01-31 13:42:55 +0300

    Home work seminar 9

---
+++ HomeWorkSeminar9/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+/*
+void ShowNaturalNumbsBetweenNandOne(int n)
+{
+    Console.Write($"{n} ");
+    if (n > 1) ShowNaturalNumbsBetweenNandOne(n-1);    
+}
+
+
+Console.Write("Input a numb: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+ShowNaturalNumbsBetweenNandOne(numb);
+*/
+
+/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetSumOfNaturalNumbsBetweenTwoNumbs(int firstNumb, int secondNumb)
+{
+    if (firstNumb > secondNumb) return firstNumb + GetSumOfNaturalNumbsBetweenTwoNumbs(firstNumb - 1, secondNumb);
+    if (firstNumb < secondNumb) return firstNumb + GetSumOfNaturalNumbsBetweenTwoNumbs(firstNumb + 1, secondNumb);
+    return firstNumb;
+}
+
+/*
+Console.Write("Input first numb: ");
+int firstNumb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second numb: ");
+int secondNumb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of numb between {firstNumb} and {secondNumb} is {GetSumOfNaturalNumbsBetweenTwoNumbs(firstNumb, secondNumb)}");
+*/
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+//По определению Рожа Петер
+
+//A(0,m)=m+1
+//A(n+1,0)=A(n,1)
+//A(n+1,m+1)=A(n,A(n+1,m))
+
+int GetResultArrermanFunc(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (m == 0) return GetResultArrermanFunc(n - 1, 1);
+    return GetResultArrermanFunc(n - 1, GetResultArrermanFunc(n, m - 1));
+}
+
+Console.WriteLine("Сalculating the Akkerman function.");
+Console.Write("Input first numb: ");
+int firstNumb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second numb: ");
+int secondNumb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetResultArrermanFunc(firstNumb,secondNumb));
---
+++ Seminar9/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+void ShowNubersNatural(int n)
+{
+    if (n > 1) ShowNubersNatural(n - 1);
+    Console.Write($"{n} ");
+}
+
+//ShowNubersNatural(5);
+
+
+
+//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int GetSumOfDigitsOfNumber(int numb)
+{
+    if (numb != 0) return GetSumOfDigitsOfNumber(numb/10) + numb % 10;
+    else return 0;
+}
+
+//Console.WriteLine(GetSumOfDigitsOfNumber(12345));
+
+//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+void ShowNubersNaturalBetweenTwoNumbers(int firstNumb, int secondNumb)
+{
+    Console.Write($"{firstNumb} ");
+    if (firstNumb > secondNumb) ShowNubersNaturalBetweenTwoNumbers(firstNumb - 1, secondNumb);
+    if (firstNumb < secondNumb) ShowNubersNaturalBetweenTwoNumbers(firstNumb + 1, secondNumb);    
+}
+
+//ShowNubersNaturalBetweenTwoNumbers(10, 5);
+
+//Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+double MyPow(double numb, int pow)
+{
+    if (pow > 0) return numb * MyPow(numb, pow - 1);
+    if (pow < 0) return (1/numb) * MyPow(numb, pow + 1);
+    return 1;
+}
+
+Console.WriteLine(MyPow(2, -3));
+
